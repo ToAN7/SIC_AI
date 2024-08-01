f3c969927e1d471528df04b579ecec04b9f45894
khaquachtrong74 <152188603+khaquachtrong74@users.noreply.github.com>
2024-08-01 18:26:31 +0700

    Update tach_rong.py

--- Project/tach_rong.py
+++ Project/tach_rong.py
@@ -1,5 +1,6 @@
 # t.csv
 import pandas as pd
+import re
 df_t = pd.read_csv("t.csv", delimiter="|")
 
 # print(df_t['Date'].astype(str))
@@ -7,6 +8,7 @@ numb = -1
 lt = []
 #Trước khi tách
 print(df_t.shape)
+
 for txt in df_t['Date']:
     for x in txt:
         
@@ -25,18 +27,24 @@ for i in lt:
 # print(df_t.head(10))
 # print("Cuoi")
 # print(df_t.tail(30))
-df_t.reset_index(drop=True, inplace=True)
 #Sau khi tách Date chứa "{}"
+df_t.reset_index(drop=True, inplace=True)
 print(df_t.shape)
 lt_content = []
 for idx,txt in enumerate(df_t['Content']):
     try:
-        if txt.__contains__("nan|Nan|NaN|"):
-            lt_content.append(idx)
+        if len(txt) < 100:
+            print(1)
     except:
+        lt_content.append(idx)
         pass
-    
+    else:
+        pass
+print(lt_content)
 for i in lt_content:
     df_t = df_t.drop(i)
 #Sau khi tách Content chứa "nan"
+df_t.reset_index(drop=True, inplace=True)
 print(df_t.shape)
+print(df_t[0:40])
+print(df_t[40:92])
